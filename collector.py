7eff67b51767e5f4b81b124bf6a49f342295c852
heqing90 <heqing_90@outlook.com>
2021-10-10 13:11:35 +0800

    update request headers.

--- collector.py
+++ collector.py
@@ -7,13 +7,13 @@ def print_json(obj):
     print(json.dumps(obj, indent=4, separators=(",", ":")))
 
 
-base_query_url = """http://www.cwl.gov.cn/cwl_admin/kjxx/findDrawNotice?name=ssq&issueCount=&issueStart=&issueEnd=&dayStart={}&dayEnd={}"""
+base_query_url = """http://www.cwl.gov.cn/cwl_admin/front/cwlkj/search/kjxx/findDrawNotice?name=ssq&issueCount=&issueStart=&issueEnd=&dayStart={}&dayEnd={}"""
 headers = {
     "Accept": "application/json, text/javascript, */*; q=0.01",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "zh-CN,zh;q=0.9",
     "Connection": "keep-alive",
-    "Cookie": "UniqueID=0zTYYQZui07HNSBz1576828876029; Sites=_21; _ga=GA1.3.133741631.1576828928; _gid=GA1.3.452061671.1576828928; 21_vq=2; _Jo0OQK=636F4623EEF353308AAC44FA21F9F33C6D00E2A7F7D3F28837193E3DFFE657B5BCD5F14899E487D1BCACCEAD2AB862703A8499B4C2F82C974EAAB5E61EA0319BA75F1B3C19C5B2FC5F8E6E66EDA7420CD4BE6E66EDA7420CD4B5B675FB7B14551AD3328EEBACF9E4A3CGJ1Z1Vw==",
+    "Cookie": "HMF_CI=f59a5e9c98b5baa2b595275463fc77617c2c6d4297cb33199777194a694b61c677; 21_vq=7",
     "Host": "www.cwl.gov.cn",
     "Referer": "http://www.cwl.gov.cn/kjxx/ssq/kjgg/",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36",
